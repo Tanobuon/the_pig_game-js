0adb6239f8f635580464e5d44184b6c48ec09699
TanoBuon <tano.buon.it@hotmail.com>
2023-03-04 09:59:00 +1100

    points to win set to 100

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ btnHold.addEventListener('click', function () {
 
     // 2. check if player's score is >=100
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
